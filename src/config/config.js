93d44844827c7b1da77fce0b1f146ac684cf21ed
danranshizhi <2287362952@qq.com>
2019-08-16 14:42:07 +0800

    'header头像替换'

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-import logoImg from '@images/logo.png'
+import logoImg from '@images/logo.jpg'
 import one from '@images/one.png'
 import two from '@images/two.png'
 import three from '@images/three.png'
